21a15bc57eea18d784c92f39aba14f4724a689df
Anton Reshetnikov <120567013+arshtnkv@users.noreply.github.com>
2024-03-26 14:31:32 +0300

    удаляет сферу, отключает модалку, отключает карту

--- source/js/main.js
+++ source/js/main.js
@@ -1,26 +1,24 @@
 import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
+// import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 import {headerFixed} from './modules/header-fixed';
 import {initPreloader} from './modules/preloader';
 import {initMenu} from './modules/init-menu';
 import {initNavigationChanger} from './modules/init-navigation-changer';
-import {initSphere} from './modules/init-sphere';
-import {initMapBlock} from './modules/init-map';
+// import {initMapBlock} from './modules/init-map';
 import {initMoreItemOnclick} from './modules/init-more-item-onclick';
 
 window.addEventListener('DOMContentLoaded', () => {
   iosVhFix();
   headerFixed();
-  initSphere();
   initMenu();
   initNavigationChanger();
   initMoreItemOnclick();
 
   window.addEventListener('load', () => {
     initPreloader();
-    initMapBlock();
-    initModals();
+    // initMapBlock();
+    // initModals();
     const form = new Form();
     window.form = form;
     form.init();
